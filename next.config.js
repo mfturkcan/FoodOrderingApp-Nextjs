ab3f7521aa0b20490fb063290e5734193bc834fe
mfturkcan <bymehmet45tr@gmail.com>
2021-09-22 23:37:36 +0300

    Fix env values issue

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   env: {
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    URL: process.env.URL
   }
 }
