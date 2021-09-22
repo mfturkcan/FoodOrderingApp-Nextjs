f15459f601616f4f948dc0ffdc1dda7b0c1341c1
mfturkcan <bymehmet45tr@gmail.com>
2021-09-22 22:52:22 +0300

    Fix typo issue in import

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,7 +1,7 @@
-import { loadStripe} from "@stripe/stripe-js";
-import { Elements, useStripe, useElements, CardElement, ElementsConsumer} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements, useStripe, useElements, CardElement, ElementsConsumer } from "@stripe/react-stripe-js";
 import { Button } from "react-bootstrap";
-import NavigationBar from "../components/partials/Navigationbar";
+import NavigationBar from "../components/partials/NavigationBar";
 import OrderProvider, { useOrder } from "../contexts/orderContext";
 import { Form } from 'react-bootstrap';
 import Receipt from "../components/Receipt";
@@ -10,15 +10,15 @@ import axios from "axios";
 const Checkout = props => {
     const stripe = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
 
-    return(
+    return (
         <div>
             <NavigationBar />
-                <div  style={{ display:"flex", flexDirection:"row", margin:"20px 40px", width: "100%"}}>
-                    <Receipt style={{margin: "0px 40px"}}/>
-                    <Elements stripe={stripe}>
-                        <InjectedCheckoutForm />
-                    </Elements>
-                </div>
+            <div style={{ display: "flex", flexDirection: "row", margin: "20px 40px", width: "100%" }}>
+                <Receipt style={{ margin: "0px 40px" }} />
+                <Elements stripe={stripe}>
+                    <InjectedCheckoutForm />
+                </Elements>
+            </div>
         </div>
     );
 }
@@ -29,10 +29,10 @@ const MyCheckoutForm = props => {
     const elements = useElements();
     console.log(stripe);
 
-    const {order: [order, handlePrice]} = useOrder(useOrder);
-    function onChange(){}
+    const { order: [order, handlePrice] } = useOrder(useOrder);
+    function onChange() { }
 
-    async function handleSubmit (event){
+    async function handleSubmit(event) {
         event.preventDefault();
 
         const orderData = order.map(o => {
@@ -43,47 +43,47 @@ const MyCheckoutForm = props => {
         });
 
         console.log(orderData);
-        let {data} = await axios.post("http://localhost:3000/api/checkout_sessions", {
+        let { data } = await axios.post("http://localhost:3000/api/checkout_sessions", {
             orderList: orderData,
         });
         console.log(data);
     }
 
-    return(
-            <Form onSubmit={handleSubmit}>
+    return (
+        <Form onSubmit={handleSubmit}>
             <Form.Group style={{ display: "flex" }}>
                 <div style={{ flex: "0.90", marginRight: 10 }}>
-                <Form.Label>Address</Form.Label>
-                <input name="address" onChange={onChange} />
+                    <Form.Label>Address</Form.Label>
+                    <input name="address" onChange={onChange} />
                 </div>
             </Form.Group>
             <Form.Group style={{ display: "flex" }}>
                 <div style={{ flex: "0.65", marginRight: "6%" }}>
-                <Form.Label>City</Form.Label>
-                <input name="city" onChange={onChange} />
+                    <Form.Label>City</Form.Label>
+                    <input name="city" onChange={onChange} />
                 </div>
-                <div style={{ flex: "0.25", marginRight: 10, width:"100px" }}>
-                <Form.Label>State</Form.Label>
-                <input name="state" onChange={onChange} />
+                <div style={{ flex: "0.25", marginRight: 10, width: "100px" }}>
+                    <Form.Label>State</Form.Label>
+                    <input name="state" onChange={onChange} />
                 </div>
             </Form.Group>
-            <Form.Group style={{ width: "500px", margin:" 40px 40px"}}>
+            <Form.Group style={{ width: "500px", margin: " 40px 40px" }}>
                 <CardElement options={{
-                style: {
-                base: {
-                    fontSize: '16px',
-                    color: '#424770',
-                    '::placeholder': {
-                    color: '#aab7c4',
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
                     },
-                    },
-                    invalid: {
-                    color: '#9e2146',
-                    },
-                },
-                }}/>
+                }} />
             </Form.Group>
-            <Button type="submit" variant="outline-primary" disabled={!stripe} style={{ margin: "10px 40px"}}>Pay</Button>
+            <Button type="submit" variant="outline-primary" disabled={!stripe} style={{ margin: "10px 40px" }}>Pay</Button>
         </Form>
     );
 }
@@ -91,12 +91,12 @@ const MyCheckoutForm = props => {
 
 const InjectedCheckoutForm = () => {
     return (
-    <ElementsConsumer>
-        {({elements, stripe}) => (
-            <MyCheckoutForm elements={elements} stripe={stripe} />
-        )}
-    </ElementsConsumer>
+        <ElementsConsumer>
+            {({ elements, stripe }) => (
+                <MyCheckoutForm elements={elements} stripe={stripe} />
+            )}
+        </ElementsConsumer>
     );
-    };
+};
 
 export default Checkout;
